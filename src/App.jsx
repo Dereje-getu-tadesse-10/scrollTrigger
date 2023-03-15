ad0b8f2104929dcc89edc3dc784ab6121046402d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-03-15 11:34:55 +0100

    update: data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>
-        Hello
-      </h1>
+
     </div>
   )
 }
